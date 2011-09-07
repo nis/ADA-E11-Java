07a68f2bd52836e2e043c53fe9256833cc123cf5
Nis Sarup <nis@sarup.dk>
2011-09-07 20:10:29 +0200

    Algorithm now properly divides and conquers. Will not search folders with a "." in them. Which is used a lot on Mac.

--- FindFileOfType/FindFileOfType.java
+++ FindFileOfType/FindFileOfType.java
@@ -1,34 +1,35 @@
 import java.io.File;
 
 public class FindFileOfType {
-	public int number_of_files_found = 0;
-	public String filetype;
 	
 	public static void main (String[] args) {
 		FindFileOfType my_class = new FindFileOfType();
 		my_class.search_for_files("mp3");
 	}
 	
-	public void search_for_files(String e) {
-		filetype = e.toLowerCase();
-		System.out.println("Searching for: \"" + filetype + "\".");
-		
+	private void search_for_files(String e) {
+		System.out.println("Searching for: \"" + e + "\".");
 		long time = System.currentTimeMillis();
-		search_dir(new File("/Users/tamen/Desktop/mp3test"));
+		int number_of_files_found = search_dir(new File("/Users/nis"), e.toLowerCase());
 		time = System.currentTimeMillis() - time;
-		System.out.println("Found " + number_of_files_found + " files of type \"" + filetype + "\".");
+		System.out.println("Found " + number_of_files_found + " files of type \"" + e + "\".");
 		System.out.println("The search took " + time/1000 + "s.");
 	}
 	
-	public void search_dir(File p) {
+	private int search_dir(File p, String filetype) {
 		File[] files = p.listFiles();
-		
-		for (int i = 0; i < files.length; i++) {
-			if (files[i].isFile() && files[i].getName().toLowerCase().endsWith(filetype)) {
-				number_of_files_found++;
-			} else if (files[i].isDirectory() && !files[i].getName().startsWith(".")) { // Directory
-				search_dir(new File(files[i].getAbsolutePath()));
+		int result = 0;
+		if (files.length > 0) {
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].isFile() && files[i].getName().toLowerCase().endsWith(filetype)) {
+					result++;
+				} else if (	files[i].isDirectory() && 
+							!files[i].isHidden() && 
+							files[i].getName().toLowerCase().indexOf(".") == -1) {
+					result += search_dir(new File(files[i].getAbsolutePath()), filetype);
+				}
 			}
 		}
+		return result;
 	}
 }
